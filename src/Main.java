db5a24f0e886a04b82d3cac3e398cf4d85e6877c
Rohit <r4rohit002@gmail.com>
2022-05-25 23:58:06 -0400

    Added imperative way to filter females from people

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,59 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("This is Java function programming test");
+
+        new Main().filterFemales();
+
+    }
+
+
+
+    public void filterFemales(){
+
+        List<Person> persons = List.of(
+                new Person("Rohit", Gender.MALE),
+                new Person("Alice", Gender.FEMALE),
+                new Person("Alex", Gender.MALE),
+                new Person("Amanda", Gender.FEMALE),
+                new Person("Susan", Gender.FEMALE));
+
+        //Imperative way to count
+        List<Person> females = new ArrayList<>();
+
+        for(Person person: persons){
+
+            System.out.println(person.name+" "+person.gender);
+
+            if(person.gender.equals(Gender.FEMALE)){
+                females.add(person);
+            }
+        }
+
+        for(Person female: females){
+            System.out.println(female.name);
+        }
+
+
+
+    }
+
+
+    static class Person{
+
+        public String name;
+        public Gender gender;
+
+        Person(String name, Gender gender){
+            this.name = name;
+            this.gender = gender;
+        }
+    }
+
+    enum Gender{
+        MALE, FEMALE;
     }
 
 }
